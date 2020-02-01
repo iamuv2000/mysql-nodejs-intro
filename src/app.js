593315b27f80e918d8e9608421fbe4bd6527cbaa
Yuvraj Singh <yuvraj.singh2018@vitstudent.ac.in>
2020-02-01 12:26:27 +0530

    SQL conection created

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
 const express =  require('express');
-const mysql = require('mysql')
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+const chalk = require('chalk');
+const bodyParser = require('body-parser')
+dotenv.config();
+
+//MySQL Configuration object
+const db = mysql.createConnection({
+    url:"localhost",
+    user:'root',
+    password:'123456789',
+    database:'TEST',
+    insecureAuth:true
+})
+
+//Connecting Database
+db.connect((e)=>{
+    if(e){
+        console.log(chalk.red("Connection "+e));
+    }
+    else{
+        console.log(chalk.green("MySQL Connected!"))
+    }
+})
 
 const app = express();
-const PORT = 3000;
+app.use(bodyParser.json());
 
-app.listen(PORT,()=>{
-    console.log("Listening on "+PORT)
+app.listen(process.env.PORT,()=>{
+    console.log("Listening on "+process.env.PORT);
 })
